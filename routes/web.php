03cd1d78a5fa7c42f4b577940a3cb8f490ec1f38
kaizen2112 <tasrif701@gmail.com>
2024-04-19 01:54:34 +0600

    Admin authentication and Admin dashboard completed

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\MerchController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TourController;
+use App\Http\Controllers\MerchController;
+use App\Http\Controllers\admin\HomeController;
 use App\Http\Controllers\MusicstoreController;
+use App\Http\Controllers\admin\AdminLoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +35,28 @@ Route::get('/store', [MerchController::class, 'index']);
 // Route::get('/store/albums/{merchs}', [MerchController::class, 'show']);
 
 Route::get('/store/albums/{id}', [MerchController::class, 'showalbum']);
+
+
+
+Route::group(['prefix'=>'admin'], function() {
+
+    Route::group(['middleware' => 'admin.guest'], function() {
+
+         Route::get('/login', [AdminLoginController::class, 'index'])->name('admin.login');
+         Route::post('/authenticate', [AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+
+
+    });
+
+    Route::group(['middleware' => 'admin.auth'], function() {
+
+        Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
+        Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
+
+
+    });
+
+});
 
 
 
